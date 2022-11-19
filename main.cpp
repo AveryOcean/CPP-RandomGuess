1aaf73b0fb420909370423a26412723307411c0a
AveryOcean <averytoad65@gmail.com>
2022-11-19 16:51:27 +0100

    Remove program telling user correct guess, add more comments

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ int main() {
 }
 
 void StartLevel(int level) {
+    // Create variables
     int max = 10 * level;
     int guess = 0;
     int tries = 0;
@@ -24,10 +25,10 @@ void StartLevel(int level) {
     std::cout << "Guess a number between 1 and " << max << std::endl;
     std::cout << "You have 5 tries" << std::endl;
     std::cout << "Enter your guess: ";
-    std::cout << "TEST: " << answer << std::endl;
 
     while(tries < 5)
     {
+        // Try to get a valid guess
         try {
             std::cin >> guess;
             tries++;
@@ -37,6 +38,7 @@ void StartLevel(int level) {
             continue;
         }
 
+        // Check if guess is correct
         if(guess == answer)
         {
             std::cout << "You win!" << std::endl;
@@ -52,12 +54,15 @@ void StartLevel(int level) {
         }
     }
 
+    // Check if player ran out of tries
     if(tries == 5)
     {
+        // Game over
         GameOver();
         return;
     }
 
+    // Proceed to the next level
     std::cout << "You guessed it in " << tries << " tries" << std::endl;
     std::cout << "Proceed to the next level" << std::endl;
 
@@ -71,14 +76,17 @@ void StartLevel(int level) {
 
 void SelectLevel() {
     int level = 1;
+
     std::cout << "Select Level to start on" << std::endl;
     std::cout << "A level determines how high the maximum number to guess is" << std::endl;
     std::cout << "Level must have a minimum value of 1" << std::endl;
 
+    // Try to get a valid level
     do {
         std::cin >> level;
     } while (level < 1);
 
+    // Start game on selected level
     std::cout << "You selected level " << level << std::endl;
     StartLevel(level);
 }
@@ -93,6 +101,7 @@ void GameOver() {
     // Make answer lowercase
     answer = tolower(answer);
 
+    // Check if player wants to play again
     if (answer == 'y') {
         StartLevel(1);
     } else {
